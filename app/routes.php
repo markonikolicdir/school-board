37ec4ceb822031d0b50aab765a74bd211b1f7f1b
Marko <markonikolicdir@gmail.com>
2020-06-09 12:03:12 +0200

    Comments to every route

--- app/routes.php
+++ app/routes.php
@@ -9,17 +9,19 @@
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
-
+//save new student, random set name and grade
 $student = new Route(
     '/student',
     array('controller' => 'StudentController', 'method'=>'save')
 );
 
+//list of all students
 $students = new Route(
     '/students',
     array('controller' => 'StudentController', 'method'=>'list')
 );
 
+//check if student pass
 $pass = new Route(
     '/student/{id}',
     array('controller' => 'StudentController', 'method'=>'pass'),
